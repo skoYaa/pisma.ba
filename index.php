f5a3daceaca5954cc0ed848474645acd60755ded
skoYaa <skoya85@gmail.com>
2015-05-06 12:33:39 +0200

    ispravljena linija, tacka zarez

--- index.php
+++ index.php
@@ -3,6 +3,6 @@ echo 'test test';
 //komentar vako nako
 echo 'test Dino';
 //Dino upisao
-echo 'faruk je bio ovdje ;D'
+echo 'faruk je bio ovdje ;D'; //zaboravio si stavit tacka zares na kraju :)
 
 
